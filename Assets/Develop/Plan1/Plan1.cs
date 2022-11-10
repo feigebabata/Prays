6b8a2044f6309171b575ea6381ca0071894e0a32
muhaijie <944236981@qq.com>
2022-11-10 18:28:54 +0800

    1.添加功德2.0版本 放置加养成 2.切换到unity2022.2 使用dots1.0开发

--- Assets/Develop/Plan1/Plan1.cs
+++ Assets/Develop/Plan1/Plan1.cs
@@ -4,82 +4,77 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class Plan1 : MonoBehaviour
+namespace Plan1
 {
-    public GameObject Tip;
-    public Animator MuYuAnimator;
-    private float _downTime;
-    private Coroutine _loopPlayKnock;
-
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
-    void Awake()
+    public class Plan1 : MonoBehaviour
     {
-        Tip.SetActive(false);
-    }
+        public GameObject Tip;
+        public Animator MuYuAnimator;
+        private float _downTime;
+        private Coroutine _loopPlayKnock;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        playKnock();
-    }
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake()
+        {
+            Tip.SetActive(false);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
+        // Start is called before the first frame update
+        void Start()
         {
             playKnock();
-            _downTime = Time.time;
-            if(_loopPlayKnock!=null)
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
             {
-                StopCoroutine(_loopPlayKnock);
-                _loopPlayKnock=null;
+                playKnock();
+                _downTime = Time.time;
+                if(_loopPlayKnock!=null)
+                {
+                    StopCoroutine(_loopPlayKnock);
+                    _loopPlayKnock=null;
+                }
+            }
+
+            if(Input.GetMouseButton(0) && Time.time-_downTime>1 && _loopPlayKnock==null)
+            {
+                _loopPlayKnock = StartCoroutine(loopPlayKnock());
             }
         }
 
-        if(Input.GetMouseButton(0) && Time.time-_downTime>1 && _loopPlayKnock==null)
+        private IEnumerator loopPlayKnock()
         {
-            _loopPlayKnock = StartCoroutine(loopPlayKnock());
+            while (true)
+            {
+                yield return new WaitForSeconds(1f/2);
+                playKnock();
+            }
         }
-    }
 
-    private IEnumerator loopPlayKnock()
-    {
-        while (true)
+        private void playKnock()
         {
-            yield return new WaitForSeconds(1f/2);
-            playKnock();
+            MuYuAnimator.gameObject.SetActive(false);
+            MuYuAnimator.gameObject.SetActive(true);
+            AudioManager.I.PlayKnock();
+            var tip = GameObject.Instantiate(Tip,Tip.transform.parent);
+            tip.SetActive(true);
+            StartCoroutine(delayDestroy(tip));
         }
-    }
 
-    private void playKnock()
-    {
-        MuYuAnimator.gameObject.SetActive(false);
-        MuYuAnimator.gameObject.SetActive(true);
-        AudioManager.I.PlayKnock();
-        var tip = GameObject.Instantiate(Tip,Tip.transform.parent);
-        tip.SetActive(true);
-        StartCoroutine(delayDestroy(tip));
-    }
+        private IEnumerator delayDestroy(GameObject tip)
+        {
+            yield return new WaitForSeconds(1f);
+            GameObject.Destroy(tip);
+        }
 
-    private IEnumerator delayDestroy(GameObject tip)
-    {
-        yield return new WaitForSeconds(1f);
-        GameObject.Destroy(tip);
-    }
-
-    public void OnPlayOM(bool isOn)
-    {
-        AudioManager.I.OnPlayOM(isOn);
+        public void OnPlayOM(bool isOn)
+        {
+            AudioManager.I.OnPlayOM(isOn);
+        }
     }
 }
-/*
-沙弥(初入 草庵)
-比丘(和尚 小庙 开始塑像1个)
-主持(人多之后管事的 寺 塑佛 菩萨 罗汉 天尊)
-方丈(精神领袖)
-长老(退隐潜修)
-佛(最高级)
-*/
